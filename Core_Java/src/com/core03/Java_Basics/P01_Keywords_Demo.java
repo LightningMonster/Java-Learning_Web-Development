7efc0fd6fe0f6ac9af9ff2170fec4568a6203c0a
Yash Dhadve <lightningmonster.1548@gmail.com>
2025-06-20 20:52:31 +0530

    Add P02_Primitive_Data_Types.java

---
+++ Core_Java/src/com/core03/Java_Basics/P01_Keywords_Demo.java
@@ -0,0 +1,18 @@
+package com.core03.Java_Basics;
+//Java Program to demonstrate Keywords
+public class P01_Keywords_Demo {
+	 public static void main(String[] args)
+	    {
+	      	// Using final and int keyword
+	        final int x = 10;
+	      
+	      	// Using if and else keywords
+	      	if(x > 10){
+	      		System.out.println("Failed");
+	        }
+	      	else {
+	           System.out.println("Successful demonstration"
+	                              +" of keywords.");
+	        }
+	    }
+}
