30263f8fcb3a464de5d65cf20e73230173eddc14
Yash Dhadve <lightningmonster.1548@gmail.com>
2025-07-02 14:15:54 +0530

    Add filtered resources to project configurations and implement Q1 class for addition of three numbers

--- Core_Java/src/com/practice/Set1/Q1.java
+++ Core_Java/src/com/practice/Set1/Q1.java
@@ -1,4 +1,12 @@
+//Addition of three numbers
 package src.com.practice.Set1;
 
 public class Q1 {
+    public static void main(String[] args){
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+
+        System.out.printf("%d + %d + %d = %d", num1,num2,num3,(num1+num2+num3));
+    }
 }
