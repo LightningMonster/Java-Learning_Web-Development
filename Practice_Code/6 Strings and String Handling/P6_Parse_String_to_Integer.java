b6ae74277406ad4b6bddf5d4bc36585846369fd0
Yash Dhadve <lightningmonster.1548@gmail.com>
2025-07-12 21:59:59 +0530

    Practice set: 6. String and String handling Practice Question: 6. Parse String to Integer

---
+++ Practice_Code/6 Strings and String Handling/P6_Parse_String_to_Integer.java
@@ -0,0 +1,19 @@
+/*
+6. Parse String to Integer
+🔸 Problem:
+Given a string "100", convert it to integer and add 50 to it.
+
+🔸 Expected Output:
+
+Result: 150
+🧠 Use Integer.parseInt()
+
+*/
+public class P6_Parse_String_to_Integer {
+    public static void main(String[] args) {
+        String str = "100";
+        int num = Integer.parseInt(str);
+        int result = num + 50;
+        System.out.println("Result: " + result);
+    }
+}
