9f207307984f247404d4f12be9a4c49042004b5a
Yash Dhadve <lightningmonster.1548@gmail.com>
2025-07-15 08:23:47 +0530

    Lesson: 8. Exception Handling Topic: 2 try, catch, finally, throw, throws

---
+++ Practice_Code/7 Object Oriented Programming/P2_1_Constructors.java
@@ -0,0 +1,12 @@
+// 2. Constructors
+/*
+* Q3. Create a Book class with a default constructor and
+* a parameterized constructor. */
+
+public class P2_1_Constructors {
+    public static void main(String[] args) {
+
+    }
+
+    static class book{}
+}
